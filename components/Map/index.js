b3b962cda461dfda39c19a275c214f1b0cddb50b
Christopher Dale <chrisdale44@gmail.com>
2022-06-19 23:05:21 +0100

    leaflet-canvas-marker working but zoom transition janky

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,9 +1,27 @@
 import React from "react";
 import { MapContainer, TileLayer } from "react-leaflet";
 import SearchField from "./SearchField";
+import LeafletCanvasMarker from "./LeafletCanvasMarker";
 import "leaflet/dist/leaflet.css";
 
 const Map = ({ spots }) => {
+  //   const markers =
+  //     spots &&
+  //     spots.map((spot, i) => (
+  //       <Marker
+  //         position={spot.coordinates}
+  //         key={i}
+  //         properties={{
+  //           key: i,
+  //           latitude: spot.coordinates[0],
+  //           longitude: spot.coordinates[1],
+  //         }}
+  //         icon={defaultIcon}
+  //       >
+  //         <Tooltip spot={spot} />
+  //       </Marker>
+  //     ));
+  console.log(spots);
   return (
     <MapContainer
       center={[51.505, -0.09]}
@@ -17,6 +35,7 @@ const Map = ({ spots }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
+      <LeafletCanvasMarker spots={spots} />
     </MapContainer>
   );
 };
