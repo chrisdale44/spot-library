60d89876ecd55a98004b5f56ea90ef4189e68f93
Christopher Dale <chrisdale44@gmail.com>
2022-06-20 02:49:08 +0100

    use react-leaflet-pixi-overlay for markers layer

--- components/Map/index.js
+++ components/Map/index.js
@@ -1,27 +1,23 @@
 import React from "react";
+import { renderToString } from "react-dom/server";
 import { MapContainer, TileLayer } from "react-leaflet";
 import SearchField from "./SearchField";
-import LeafletCanvasMarker from "./LeafletCanvasMarker";
+import PixiOverlay from "react-leaflet-pixi-overlay";
 import "leaflet/dist/leaflet.css";
 
 const Map = ({ spots }) => {
-  //   const markers =
-  //     spots &&
-  //     spots.map((spot, i) => (
-  //       <Marker
-  //         position={spot.coordinates}
-  //         key={i}
-  //         properties={{
-  //           key: i,
-  //           latitude: spot.coordinates[0],
-  //           longitude: spot.coordinates[1],
-  //         }}
-  //         icon={defaultIcon}
-  //       >
-  //         <Tooltip spot={spot} />
-  //       </Marker>
-  //     ));
-  console.log(spots);
+  const markers = spots.map((spot) => {
+    return {
+      id: spot.id,
+      position: spot.coordinates,
+      popup: renderToString(<div>Hello</div>),
+      //   iconColor: "#187bcd",
+      iconId: "someIDUniqueToIcon",
+      customIcon:
+        '<svg style="-webkit-filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .4));filter: drop-shadow( 1px 1px 1px rgba(0, 0, 0, .4));" xmlns="http://www.w3.org/2000/svg" fill="red" width="36" height="36" viewBox="0 0 24 24"><path d="M12 0c-4.198 0-8 3.403-8 7.602 0 6.243 6.377 6.903 8 16.398 1.623-9.495 8-10.155 8-16.398 0-4.199-3.801-7.602-8-7.602zm0 11c-1.657 0-3-1.343-3-3s1.342-3 3-3 3 1.343 3 3-1.343 3-3 3z"/></svg>',
+    };
+  });
+  console.log(markers);
   return (
     <MapContainer
       center={[51.505, -0.09]}
@@ -35,7 +31,7 @@ const Map = ({ spots }) => {
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <LeafletCanvasMarker spots={spots} />
+      <PixiOverlay markers={markers} />
     </MapContainer>
   );
 };
