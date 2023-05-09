80b8a64ca85931880b6f6d4101cc9ce310c0df8a
Christopher Dale <chrisdale44@gmail.com>
2023-05-09 23:22:24 +0800

    port redis db to Aiven

--- src/utils/connectToRedis.js
+++ src/utils/connectToRedis.js
@@ -2,11 +2,6 @@ import Redis from "ioredis";
 
 // TODO: reuse any existing redis connection or this isn't an issue in production?
 
-const connectToRedis = () =>
-  new Redis({
-    port: process.env.REDIS_PORT,
-    host: process.env.REDIS_URL,
-    password: process.env.REDIS_PASSWORD,
-  });
+const connectToRedis = () => new Redis(process.env.REDIS_URL);
 
 export default connectToRedis;
