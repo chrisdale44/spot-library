052755604fe6763ad360a2e3493f66d808a78c08
Christopher Dale <chrisdale44@gmail.com>
2023-05-11 11:39:07 +0800

    make tab content consistent height

--- src/components/Tabs/index.js
+++ src/components/Tabs/index.js
@@ -7,34 +7,40 @@ const Tabs = ({ headings, children }) => {
 
   return headings?.length ? (
     <div className={styles.tabs}>
-      {headings.map((heading, i) => (
-        <React.Fragment key={i}>
-          <input
-            className={styles.radiotab}
-            name="tabs"
+      <div className={styles.tabHeadings}>
+        {headings.map((heading, i) => (
+          <React.Fragment key={i}>
+            <input
+              className={styles.radiotab}
+              name="tabs"
+              tabIndex="1"
+              type="radio"
+              defaultChecked={checkedTab === i}
+              id={"tab-" + i}
+            />
+            <label
+              className={cx(styles.label, {
+                [styles.checked]: checkedTab === i,
+              })}
+              htmlFor={"tab-" + i}
+              onClick={() => setCheckedTab(i)}
+            >
+              {heading}
+            </label>
+          </React.Fragment>
+        ))}
+      </div>
+      <div className={styles.tabContent}>
+        {children.map((child, i) => (
+          <div
+            className={cx(styles.panel, { [styles.checked]: checkedTab === i })}
             tabIndex="1"
-            type="radio"
-            defaultChecked={checkedTab === i}
-            id={"tab-" + i}
-          />
-          <label
-            className={cx(styles.label, { [styles.checked]: checkedTab === i })}
-            htmlFor={"tab-" + i}
-            onClick={() => setCheckedTab(i)}
+            key={i}
           >
-            {heading}
-          </label>
-        </React.Fragment>
-      ))}
-      {children.map((child, i) => (
-        <div
-          className={cx(styles.panel, { [styles.checked]: checkedTab === i })}
-          tabIndex="1"
-          key={i}
-        >
-          {child}
-        </div>
-      ))}
+            {child}
+          </div>
+        ))}
+      </div>
     </div>
   ) : null;
 };
