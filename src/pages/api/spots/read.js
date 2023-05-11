d4263809892c899534bb68ba93c7db29e4c32977
Christopher Dale <chrisdale44@gmail.com>
2023-05-12 00:52:35 +0800

    add console log for build debugging

--- src/pages/api/spots/read.js
+++ src/pages/api/spots/read.js
@@ -5,6 +5,7 @@ const redis = connectToRedis();
 export default async function handler(req, res) {
   try {
     const spotsHash = await redis.hgetall("spots");
+    console.log(spotsHash);
     const spotsArray = Object.keys(spotsHash).map((key) =>
       JSON.parse(spotsHash[key])
     );
